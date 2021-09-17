f526ef6df065e1e555d8d5fa4c02028ad55f0d23
akari nakamura <akarinakamura0305@gmail.com>
2021-09-17 14:17:59 +0900

    リポジトリ一覧の取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { GraphQLClient } from "graphql-request";
 
 function App() {
+  const [restData, setRestData] = useState([]);
+  const [graphqlData, setGraphqlData] = useState<any>({});
+
+  const uri = "https://api.github.com/users/an-0305/repos";
+
+  const query = `query {
+    user(login: "an-0305") {
+      repositories(last: 20) {
+        nodes {
+          name
+        }
+      }
+    }
+  }`;
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const client = new GraphQLClient("https://api.github.com/graphql", {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+    },
+  });
+
+  useEffect(() => {
+    if (!uri) return;
+    fetch(uri, {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setRestData(result);
+      })
+      .catch(console.error);
+  }, [uri]);
+
+  useEffect(() => {
+    client
+      .request(query)
+      .then((res) => {
+        setGraphqlData(res.user.repositories.nodes);
+      })
+      .catch(console.error);
+  }, [query]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>repositories</h1>
+      <div>
+        <p>use REST API</p>
+        <ul>
+          {restData.map((item: any, i: number) => (
+            <li key={i}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <p>use GraphQL</p>
+        <ul>
+          {Object.keys(graphqlData).length &&
+            graphqlData.map((item: any, i: number) => (
+              <li key={i}>{item.name}</li>
+            ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
